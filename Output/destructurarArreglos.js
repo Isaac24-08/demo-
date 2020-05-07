57e18c847332dae7d36eb5761f436a629d0fe84f
Arenas <isaacarenas53@gmail.com>
2020-05-07 16:41:06 -0500

    Se añadieron nuevas carpetas

---
+++ Output/destructurarArreglos.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var mamalo = ["Isaac", 30, 50, "Harvard", "Tqm"];
+var nombre = mamalo[0],
+    edad = mamalo[1],
+    cuantasMascotas = mamalo[2],
+    trabajo = mamalo[3],
+    sentimiento = mamalo[4];
+console.log(cuantasMascotas);
---
+++ Src/destructurarArreglos.js
@@ -0,0 +1,5 @@
+const mamalo = ["Isaac" , 30, 50, "Harvard", "Tqm"];
+
+const [nombre, edad, cuantasMascotas, trabajo, sentimiento] = mamalo;
+
+console.log(cuantasMascotas);
