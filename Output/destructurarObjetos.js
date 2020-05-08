d91cc9d77d1f19bd4ddef0b043204a01a575ec15
Arenas <isaacarenas53@gmail.com>
2020-05-07 19:13:17 -0500

    Se añadio un nuevo archivo

---
+++ Output/destructurarObjetos.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var objetoUsuario = {
+  nombre: "Isaac",
+  apellido: "Arenas",
+  edad: "17",
+  correoElectronico: "isaacarenas53@gmail.com",
+  pais: "Venezuela",
+  profesion: "Programador"
+};
+var pais = objetoUsuario.pais;
+console.log(pais);
