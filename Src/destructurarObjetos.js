b7ce1d6a8392ebf132bc09a4a08bdb4de258daaa
Arenas <isaacarenas53@gmail.com>
2020-05-07 19:11:38 -0500

    Se creo un nuevo objeto

---
+++ Src/destructurarObjetos.js
@@ -0,0 +1,15 @@
+
+const objetoUsuario = {
+
+    nombre : "Isaac",
+    apellido : "Arenas",
+    edad : "17",
+    correoElectronico : "isaacarenas53@gmail.com",
+    pais : "Venezuela",
+    profesion : "Programador",
+
+}
+
+const {pais} = objetoUsuario;
+
+console.log(pais);
